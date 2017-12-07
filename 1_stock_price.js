2f6761b18a25bf673654a44d64aff89ea866972c
robinchoii <robinchoii@gmail.com>
2017-12-06 22:23:55 -0800

    refactor, O(n)

--- 1_stock_price.js
+++ 1_stock_price.js
@@ -1,40 +1,75 @@
-var stockPricesYesterday = [16, 11, 8, 5, 2]
+var stockPricesYesterday = [104, 27, 15, 12, 11, 9]
 
 function getMaxProfit(stock_prices) {
 
     var max_profit = 0;
 
-    for (var buy_index = 0; buy_index < stock_prices.length; buy_index++) {
+    var min_price = stock_prices[0];
 
-        var buy_price = stock_prices[buy_index]
+    for (var i = 0; i < stock_prices.length; i++) {
 
-        for (var sell_index = buy_index + 1; sell_index < stock_prices.length; sell_index++) {
+        var sell_price = stock_prices[i+1];
 
-            var sell_price = stock_prices[sell_index]
+        var profit = sell_price - min_price;
 
-            var profit = sell_price - buy_price
+        if (profit > max_profit) {
 
-            if (profit > max_profit) {
-
-                max_profit = profit;
-
-            }
+            max_profit = profit
 
         }
 
+        if (sell_price < min_price) {
+
+            min_price = sell_price;
+
+
+        }
+
+
     }
 
-   if (profit <= 0 ) {
+    if( max_profit  <= 0 )  {
 
-        console.log('dont sell!')
+        console.log('error, you are selling at a loss');
+
+        return false
 
     } else {
 
         console.log(max_profit)
 
-        return max_profit
-
     }
+   //  for (var buy_index = 0; buy_index < stock_prices.length; buy_index++) {
+
+   //      var buy_price = stock_prices[buy_index]
+
+   //      for (var sell_index = buy_index + 1; sell_index < stock_prices.length; sell_index++) {
+
+   //          var sell_price = stock_prices[sell_index]
+
+   //          var profit = sell_price - buy_price
+
+   //          if (profit > max_profit) {
+
+   //              max_profit = profit;
+
+   //          }
+
+   //      }
+
+   //  }
+
+   // if (profit <= 0 ) {
+
+   //      console.log('dont sell!')
+
+   //  } else {
+
+   //      console.log(max_profit)
+
+   //      return max_profit
+
+   //  }
 
 }
 
